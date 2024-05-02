b4b49ee96c686e969006ba5c7cf7bc61961497e6
fryskin <gman247@mail.ru>
2024-05-02 23:28:34 +0200

    models of database

---
+++ lib/repositories/accounts/models/account.dart
@@ -0,0 +1,19 @@
+/*class Account {
+  const Account(
+      {required this.uid,
+      required this.serviceTypes,
+      required this.city,
+      required this.region,
+      required this.email,
+      required this.phone,
+      required this.creationTime,
+      required this.updateTime});
+  final String uid;
+  final List serviceTypes;
+  final String city;
+  final String region;
+  final String email;
+  final String phone;
+  final DateTime creationTime;
+  final DateTime updateTime;
+} */
---
+++ lib/repositories/worksheets/models/worksheet.dart
@@ -0,0 +1,18 @@
+class Worksheet {
+  const Worksheet(
+      {required this.name,
+      required this.surname,
+      required this.phoneNumber,
+      required this.email,
+      required this.address,
+      required this.serviceType,
+      required this.price});
+
+  final String name;
+  final String surname;
+  final String phoneNumber;
+  final String email;
+  final String address;
+  final String serviceType;
+  final int price;
+}
